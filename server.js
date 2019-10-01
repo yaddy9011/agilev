e8533094f637a75dd7a9aa9fbd81d71e4bf3ac8b
yaddy9011 <yaddy9011@gmail.com>
2019-09-30 19:24:54 -0500

    correcto.. ok

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ app.use(express.static(__dirname + '/dist'));
 app.all('*', (req, res) => {  
   res.status(200).sendFile(__dirname + '/dist/front/index.html');  
 });  
-app.listen(process.env.PORT || 8080); 
+app.listen(process.env.PORT || 8080);
+console.log('App escuchando');
